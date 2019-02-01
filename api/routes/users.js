027246eb16b80818dea7caefdc1b3fdc98325777
foliwe <foliwe@gmail.com>
2019-02-01 20:57:55 +0000

    jwt addded

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user");
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
 
 
 
@@ -46,6 +47,52 @@ router.post('/signup',(req,res, next)=>{
     
 })
 
+
+
+// LOGIN
+router.post("/login", (req, res, next) => {
+    User.find({ email: req.body.email })
+      .exec()
+      .then(user => {
+        if (user.length < 1) {
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+          if (err) {
+            return res.status(401).json({
+              message: "Auth failed"
+            });
+          }
+          if (result) {
+              const token = jwt.sign({
+                  email: user[0].email,
+                  userId: user[0]._id,                 
+              }, 
+              process.env.JWT_SECRET_KEY,
+               {
+                  expiresIn: '1h'
+               },
+
+              );
+            return res.status(200).json({
+              message: "Auth successful",
+               token: token
+            });
+          }
+          res.status(401).json({
+            message: "Auth failed"
+          });
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
 
 //DELETE
 router.delete('/:userId',(req,res, next) =>{
